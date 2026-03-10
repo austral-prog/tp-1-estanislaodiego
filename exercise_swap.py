9de2a1c77472e9c88f22170b141609cf4485f173
estanislaodiego <ediego@mail.austral.edu.ar>
2026-03-10 18:28:04 -0300

    Update exercise_swap.py

--- exercise_swap.py
+++ exercise_swap.py
@@ -10,3 +10,12 @@ def swap():
     """
     x = 10
     y = 20
+    valor_original_y = y
+    valor_original_x = x
+    valor_antes_y = x
+    valor_antes_x = y
+
+    print(valor_original_x)
+    print(valor_original_y)
+    print(valor_antes_x)
+    print(valor_antes_y)
